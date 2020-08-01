4bc83aea9fa8519248492f3c73da52e54a188208
Esteban Ramirez Alvarez <era-73@hotmail.com>
2020-08-01 18:12:52 -0500

    incluir instalador de base de datos

--- ReadMe.txt
+++ ReadMe.txt
@@ -4,5 +4,12 @@ A continuación, se describen los contenidos de cada carpeta:
 - WebApp:
 	contiene los archivos para el servicio web que usaran los usuarios.
 - SQL:
-	contiene los archivos que permiten la inicialización de la base de datos
-	NOTA: la base de datos usada es MariaDB
+	- install_mariadb.bat:
+		instala el servidor de base de datos con las credenciales necesarisas
+		NOTA: la base de datos usada es MariaDB
+		NOTA 2: si el archivo comunica algún error, entonces se debe instalar la base de datos con el archivo "mariadb.msi" con el usuario "root" y contraseña "admin"
+	- load_db.bat:
+		carga la base de datos inicial del archivo "db_backup_company.sql"
+		NOTA: si el archivo comunica algun error, entonces se debe cargar la base de datos manualmente usando el archivo "db_backup_company.sql"
+	- copy_db.bat:
+		copia la base de datos en el archivo "db_backup_company.sql"
--- SQL/db_backup_company.sql
+++ SQL/db_backup_company.sql
@@ -16,13 +16,123 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `usuario`
+-- Table structure for table `cost`
 --
 
-DROP TABLE IF EXISTS `usuario`;
+DROP TABLE IF EXISTS `cost`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `usuario` (
+CREATE TABLE `cost` (
+  `id_cost` int(11) NOT NULL,
+  `id_game` int(11) DEFAULT NULL,
+  `cost` float DEFAULT NULL,
+  `cost_date` date DEFAULT NULL,
+  PRIMARY KEY (`id_cost`),
+  KEY `FK__game` (`id_game`),
+  CONSTRAINT `FK__game` FOREIGN KEY (`id_game`) REFERENCES `game` (`id_game`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `cost`
+--
+
+LOCK TABLES `cost` WRITE;
+/*!40000 ALTER TABLE `cost` DISABLE KEYS */;
+/*!40000 ALTER TABLE `cost` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `game`
+--
+
+DROP TABLE IF EXISTS `game`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `game` (
+  `id_game` int(11) NOT NULL,
+  `title` varchar(30) NOT NULL,
+  `director` varchar(50) NOT NULL,
+  `protagonist` varchar(50) NOT NULL,
+  `producer` varchar(50) NOT NULL,
+  `age` int(11) NOT NULL,
+  `entry_date` date NOT NULL,
+  `technology` varchar(20) NOT NULL,
+  PRIMARY KEY (`id_game`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `game`
+--
+
+LOCK TABLES `game` WRITE;
+/*!40000 ALTER TABLE `game` DISABLE KEYS */;
+/*!40000 ALTER TABLE `game` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `rental`
+--
+
+DROP TABLE IF EXISTS `rental`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `rental` (
+  `id_rental` int(11) NOT NULL,
+  `user_document` int(11) DEFAULT NULL,
+  `rental_date` date DEFAULT NULL,
+  `status` enum('En prestamo','Vencido','Finalizado') DEFAULT NULL,
+  `cost` float DEFAULT NULL,
+  PRIMARY KEY (`id_rental`),
+  KEY `FK__user` (`user_document`),
+  CONSTRAINT `FK__user` FOREIGN KEY (`user_document`) REFERENCES `user` (`document`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `rental`
+--
+
+LOCK TABLES `rental` WRITE;
+/*!40000 ALTER TABLE `rental` DISABLE KEYS */;
+/*!40000 ALTER TABLE `rental` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `selection`
+--
+
+DROP TABLE IF EXISTS `selection`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `selection` (
+  `id_rental` int(11) NOT NULL,
+  `id_game` int(11) NOT NULL,
+  KEY `id_rental` (`id_rental`),
+  KEY `id_game` (`id_game`),
+  CONSTRAINT `FK__rental` FOREIGN KEY (`id_rental`) REFERENCES `rental` (`id_rental`),
+  CONSTRAINT `FK_selection_game` FOREIGN KEY (`id_game`) REFERENCES `game` (`id_game`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `selection`
+--
+
+LOCK TABLES `selection` WRITE;
+/*!40000 ALTER TABLE `selection` DISABLE KEYS */;
+/*!40000 ALTER TABLE `selection` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user` (
   `document` int(11) NOT NULL,
   `email` varchar(50) NOT NULL,
   `full_name` varchar(50) NOT NULL,
@@ -33,12 +143,12 @@ CREATE TABLE `usuario` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `usuario`
+-- Dumping data for table `user`
 --
 
-LOCK TABLES `usuario` WRITE;
-/*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
-/*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -58,4 +168,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-08-01 16:52:22
+-- Dump completed on 2020-08-01 17:35:01
